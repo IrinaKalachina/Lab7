edfe81efa309c9c3608b87a33729d03e734dc8b4
Irina <ira.kalachina@yandex.ru>
2023-11-15 10:06:08 +0700

    3

--- main.py
+++ main.py
@@ -103,3 +103,34 @@ def task_3():
             print(f"Можно получить {arr3[i]} из {arr2[i]} и {arr1[i]} арифметическими преобразованиями")
         else:
             print(f"Нельзя получить {arr3[i]} из {arr2[i]} и {arr1[i]} арифметическими преобразованиями")
+
+def main():
+    while True:
+        print("\nГлавное меню:")
+        print("\n1.Выбор задания\n2.Завершить программу")
+
+        choice = input("Выберите пункт меню: ")
+
+        if choice == '1':
+            print("\n1. Задание 1")
+            print("2. Задание 2")
+            print("3. Задание 3")
+
+            second_choice = input("Выберите пункт меню: ")
+            if second_choice == '1':
+                task_1()
+            elif second_choice == '2':
+                task_2()
+            elif second_choice == '3':
+                task_3()
+            else:
+                print("Неверный выбор. Пожалуйста, выберите пункт меню от 1 до 3.")
+        elif choice == '2':
+            print("Программа завершена.")
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, выберите пункт меню 1 или 2.")
+
+
+if __name__ == "__main__":
+    main()
